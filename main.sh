d5cf015dc317737b03b97d76932bfc4c1c82731b
ferrreo <harderthanfire@gmail.com>
2023-06-16 18:36:17 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,12 +2,6 @@
 
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
-
 # Clone Upstream
 wget -nv https://gitlab.freedesktop.org/libinput/libinput/-/archive/1.23.0/libinput-1.23.0.tar.gz
 tar -xf ./libinput-1.23.0.tar.gz
@@ -16,8 +10,6 @@ cp -rvf ./debian ./libinput-1.23.0/
 cd ./libinput-1.23.0/
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
